dc42683b652fe343e23b6545c6cb5a34e4b39d2c
dramollei <48841404+dramollei@users.noreply.github.com>
2022-05-24 21:24:26 +0200

    whoopsie i committed the test code by accident

--- src/main/java/me/fen/atomgame/Main.java
+++ src/main/java/me/fen/atomgame/Main.java
@@ -2,9 +2,6 @@ package me.fen.atomgame;
 
 import me.fen.atomgame.gamemodes.Gamemode;
 import me.fen.atomgame.gamemodes.NNextsGame;
-import me.fen.atomgame.particles.Atom;
-import me.fen.atomgame.particles.Minus;
-import me.fen.atomgame.particles.Plus;
 
 import java.util.List;
 import java.util.Scanner;
@@ -13,11 +10,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Gamemode g = new NNextsGame(4, new DefaultParticleRandomizer(), new DefaultScoringStrategy());
-//        g.getParticles().addAll(List.of(new Atom(3), new Plus(), new Atom(2), new Plus(), new Atom(2), new Atom(110), new Atom(110), new Atom(110)));
-        g.getParticles().addAll(List.of(new Atom(98), new Atom(3), new Atom(99), new Plus(), new Atom(3), new Atom(101)));
-        g.getNext().set(0, new Minus());
-//        Particle[] a = {new Atom(4), new Atom(3), new DarkPlus(), new Plus(), new Atom(3)};
-//        g.particles = new CircularList<>(List.of(a));
         while (!g.isGameOver()) {
             System.out.println(g);
             System.out.print("enter idx to insert after: ");
