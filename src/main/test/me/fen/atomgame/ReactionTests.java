e3ff8fd1c1138405e169c87edc100290880b6642
dramollei <48841404+dramollei@users.noreply.github.com>
2022-06-19 22:54:29 +0200

    redo test to use new weakEquals

--- src/main/test/me/fen/atomgame/ReactionTests.java
+++ src/main/test/me/fen/atomgame/ReactionTests.java
@@ -6,11 +6,30 @@ import me.fen.atomgame.particles.Particle;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static me.fen.atomgame.parse.ParticleGenerators.generateList;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ReactionTests {
+    static boolean particleListSame(List<Particle> fst, List<Particle> snd) {
+        if (fst.size() != snd.size()) return false;
+
+        class ParticlePair {
+            final Particle fst;
+            final Particle snd;
+
+            public ParticlePair(Particle fst, Particle snd) {
+                this.fst = fst;
+                this.snd = snd;
+            }
+        }
+
+        return IntStream.range(0, fst.size())
+                .mapToObj(i -> new ParticlePair(fst.get(i), snd.get(i)))
+                .allMatch(i -> i.fst.weakEquals(i.snd));
+    }
+
     static Gamemode constructGame(List<Particle> field, List<Particle> next) {
         DefaultGame game = new DefaultGame(new RepeatParticleRandomizer(next), new DefaultScoringStrategy());
         game.getParticles().addAll(field);
@@ -25,6 +44,6 @@ public class ReactionTests {
     public void barebonesReactionTest() throws GameOverException {
         Gamemode g = constructGame("1 1", "+");
         g.doMove(0);
-        assertEquals(g.getParticles(), generateList("2"));
+        assertTrue(particleListSame(g.getParticles(), generateList("2")));
     }
 }
